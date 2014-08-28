bdbe8456c26c8a3cd283cccdb66a559be42effe7
Yuriy Osadchuk <yosadtc@gmail.com>
2014-08-29 00:00:07 +0200

    first commit

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+const double pi = 3.14159;
+const char newline = '\n';
+char16_t x = 'A';
+
+int main ()
+{
+  cout << sizeof(x) << endl;
+  cout << x << endl;
+  // cout << "\x41" << endl;
+
+}
